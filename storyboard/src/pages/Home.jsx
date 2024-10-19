05a2983a6a40fdea49a02a2dbcbf9457936d8f4e
Kit-Kat31415926 <ravenkc2017@gmail.com>
2024-10-19 11:35:44 -0700

    Fix favicon

--- storyboard/src/pages/Home.jsx
+++ storyboard/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import SideBar from '../components/SideBar'
 const Home = () => {
     return (
         <div className='HomePage'>
             <p>This is the home page</p>
+            <SideBar />
         </div>
     );
 }
