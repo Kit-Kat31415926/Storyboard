31f93ac7bba34eeef50c6a97fbbef0984654963b
Kit-Kat31415926 <ravenkc2017@gmail.com>
2024-10-19 11:09:31 -0700

    Get basic page working

--- storyboard/src/App.js
+++ storyboard/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import LandingPage from './pages/LandingPage';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='Storyboard'>
+        <Routes>
+          <Route path='/LandingPage' element={<LandingPage />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ storyboard/src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div className='LandingPage'>
+            <p>This is the landing page</p>
+        </div>
+    );
+}
+
+export default LandingPage;
